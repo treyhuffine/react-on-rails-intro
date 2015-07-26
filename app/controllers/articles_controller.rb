ab9138435badd4558cbf9e079bcbd9343c3ad3d8
Trey Huffine <treyhuffine@gmail.com>
2015-07-26 10:15:49 -0700

    Create article MVC

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,2 +1,18 @@
 class ArticlesController < ApplicationController
+  def create
+    article = Article.create(article_params)
+
+    render json: article
+  end
+
+  def index
+    render json: Article.all
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :link, :language, :published_at,
+                                    :description, :author_name, :tags)
+  end
 end
